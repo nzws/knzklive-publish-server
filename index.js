958bc1223b6107d6807f20f18a36f57ff1969d64
nzws <github@nzws.me>
2018-10-13 23:21:40 +0900

    fix error / indent

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const { NodeMediaCluster, NodeMediaServer } = require('node-media-server'),
-    axios = require('axios'),
-    conf = require('./config');
+  axios = require('axios'),
+  conf = require('./config');
 
 let config = {
-    rtmp: {
-        port: 1935,
-        chunk_size: 100000,
-        gop_cache: false,
-        ping: 60,
-        ping_timeout: 30
-    },
-    http: {
-        port: conf.http_port,
-        allow_origin: '*',
-        mediaroot: './media'
-    },
-    knzklive: {
-        api_endpoint: conf.endpoint,
-        api_key: conf.APIKey
-    }
+  rtmp: {
+    port: 1935,
+    chunk_size: 100000,
+    gop_cache: false,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: conf.http_port,
+    allow_origin: '*',
+    mediaroot: './media'
+  },
+  knzklive: {
+    api_endpoint: conf.endpoint,
+    api_key: conf.APIKey
+  }
 };
 
 if (conf.https_port) {
-    config["https"] = {
-      port: conf.https_port,
-      cert: conf.https_cert,
-      key: conf.https_key,
-    };
+  config["https"] = {
+    port: conf.https_port,
+    cert: conf.https_cert,
+    key: conf.https_key,
+  };
 }
 
 if (conf.ffmpeg_path) {
@@ -47,17 +47,22 @@ const nmcs = new NodeMediaServer(config);
 nmcs.run();
 
 nmcs.on('prePublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
 });
 
 nmcs.on('donePublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  axios.get(`${config.knzklive.api_endpoint}publish.php?token=${args.token}&live=${StreamPath}&authorization=${config.knzklive.api_key}&mode=done_publish`).then(response => {
+    console.log('[donePublish]', response);
+  }).catch(error => {
+    console.log('[donePublish]', error);
+  })
 });
 
 nmcs.on('postPlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
 });
 
 nmcs.on('donePlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
 });
