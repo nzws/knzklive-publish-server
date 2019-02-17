380e5a3c548589f1e5dbcff8e03ac3e3f1ef82a6
bsky <git@bsky.moe>
2019-02-17 17:14:22 +0900

    Add lint-staged

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,6 +2,6 @@ const tasks = arr => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['pretty-quick --staged', 'yarn lint'])
+    'pre-commit': tasks(['pretty-quick --staged', 'lint-staged'])
   }
 };
